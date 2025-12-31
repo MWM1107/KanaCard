7743e87ee2c3c8820c62164ee36dcad46ffe29ce
Kevin Struna <kev.struna@gmail.com>
2025-12-31 09:54:54 -0700

    Initial Commit

---
+++ KanaCard/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  KanaCard
+//
+//  Created by Kevin Struna on 12/31/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ KanaCardTests/KanaCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  KanaCardTests.swift
+//  KanaCardTests
+//
+//  Created by Kevin Struna on 12/31/25.
+//
+
+import Testing
+@testable import KanaCard
+
+struct KanaCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
